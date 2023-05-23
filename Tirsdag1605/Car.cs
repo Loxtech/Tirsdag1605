4ea2d9c10603da4978800fec8309358448927ac7
Loke Bendtsen <lokebendtsen@gmail.com>
2023-05-23 22:03:29 +0200

    Tirsdag2305

---
+++ Tirsdag1605/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tirsdag1605
+{
+    internal class Car
+    {
+    }
+}
--- Tirsdag1605/Program.cs
+++ Tirsdag1605/Program.cs
@@ -14,6 +14,10 @@
             Animal animal2 = new Animal() { Age = 133, Color = "Purple", Name = "", FirstName = "Rich", LastName = "Manson" };
             Animal animal3 = new Animal() { Age = 20, Color = "Blue", Name = "", FirstName = "Jesper", LastName = "Sonnes" };
 
+            Car car1 = new Car() { Age = 10, Color = "Green", Name = "", Model = "A6", Maker = "Audi" };
+            Car car2 = new Car() { Age = 22, Color = "Red", Name = "", Model = "F1", Maker = "McLaren" };
+            Car car3 = new Car() { Age = 2, Color = "Black", Name = "", Model = "AMG One", Maker = "Mercedes" };
+
             //animal3.create();
             //animal1.create();
             //animal2.create();
@@ -25,6 +29,55 @@
             //Animal.Read(13);
 
             Animal.NameJumble(14);
+
+
+
+            int[] intArr = new int[3];
+            intArr[1] = 1;
+            intArr[2] = 2;
+            intArr[3] = 3;
+
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    Console.WriteLine(intArr[i]);
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Array size execced:" + e);
+                }
+            }
+
+            int a = 5;
+
+            try
+            {
+                int b = a / 0;
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Cannot divide with 0:" + e);
+            }
+
+
+            Animal animal4 = null;
+
+            try
+            {
+                animal4.create();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Animal has no value and cannot be created:" + e);
+            }
+
+
+
+
         }
 
     }
